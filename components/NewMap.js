5375c2d85f6ff52cae6cdc0957819910a9e14822
websterKAT <lakshankat@gmail.com>
2023-11-30 17:55:47 +0530

    new Map added

--- components/NewMap.js
+++ components/NewMap.js
@@ -1,9 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Loader } from "@googlemaps/js-api-loader"
+import Form from 'react-bootstrap/Form';
 
 const NewMap = () => {
   const mapRef = useRef(null);
   const dataLayerRef = useRef(null);
+  const [selectedRegion, setSelectedRegion] = useState(null);
+  const [geoData, setGeoData] = useState(null);
   let map = null;
 
   const loader = new Loader({
@@ -12,27 +15,8 @@ const NewMap = () => {
 
   const [mapLoaded, setMapLoaded] = useState(false);
 
-  const onScriptLoad = () => {
-    console.log("scripted loaded");
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: { lat: 50.5, lng: 4 },
-      zoom: 8,
-      gestureHandling: 'greedy',
-      disableDefaultUI: true,
-    });
-    controlUI.style.backgroundColor = "white";
-
-    mapRef.current = map;
-    setMapLoaded(true);
-    //dataHandler('https://storage.googleapis.com/mapsdevsite/json/google.json');
-  };
-
-
-
   const dataHandler = async (getJson) => {
-    if (!mapLoaded) {
-        console.log('inside not loadd');
-    };
+    if (!mapLoaded) return;
 
     // Remove the current layer if there is one
     if (dataLayerRef.current) {
@@ -40,59 +24,92 @@ const NewMap = () => {
     }
 
     // Fetch JSON file
-    fetch('https://prop-ai-user-data.s3.me-central-1.amazonaws.com/community.json', {
+    fetch(getJson, {
         method: "GET"
     })
-        .then(response => response.json())
-        .then(data => {
-            const newData = data.features.filter((obj) => obj.geometry.coordinates && obj.geometry.coordinates[0].length != 1)
-            console.log(newData,'daaa0', data);
-            data.features = newData;
-            const dataLayer = mapRef.current.data.addGeoJson(data)
-            dataLayerRef.current = dataLayer;
-            mapRef.current.data.setStyle({ strokeWeight: 0.5, fillOpacity: 0.1 });
-        });
+    .then(response => response.json())
+    .then(data => {
+        // filtering null values
+        const newData = data.features.filter((obj) => obj.geometry.coordinates && obj.geometry.coordinates[0].length != 1)
+        data.features = newData;
+        setGeoData(data);
+        const dataLayer = mapRef.current.data.addGeoJson(data)
+        dataLayerRef.current = dataLayer;
+        mapRef.current.data.setStyle({ strokeWeight: 0.5, fillOpacity: 0.1 });
+        
+        mapRef.current.data.addListener('mouseover', (event) => {
+          mapRef.current.data.revertStyle();
+          mapRef.current.data.overrideStyle(event.feature, { strokeWeight: 1, fillOpacity: 0.5 });
+      });
   
-      mapRef.current.data.addListener('mouseover', (event) => {
-      mapRef.current.data.revertStyle();
-      // Add a style when you hover over a specific polygon
-      mapRef.current.data.overrideStyle(event.feature, { strokeWeight: 1, fillOpacity: 0.5 });
-      // In the console log, you can see all the data you can return
-      console.log(event.feature);
+      mapRef.current.data.addListener("mouseout", (event) => {
+        mapRef.current.data.revertStyle();
+      });
+        mapRef.current.data.addListener('click', (event) => {
+          const featureProperties = event.feature.getProperty('description');
+    
+          const infoWindow = new window.google.maps.InfoWindow({
+            content: featureProperties,
+            position: event.latLng
+          });
+    
+          infoWindow.open(mapRef.current);
+        });
     });
+  };
 
-    // mapRef.current.data.addListener('mouseout', () => {
-    //   // Revert the style to how it was when you hover out
-    //   mapRef.current.data.revertStyle();
-    // });
+  const onSelectChange = (eventValue) => {
+
+    setSelectedRegion(eventValue);
+    mapRef.current.data.setStyle((feature) =>
+     {
+        let color = 'grey';
+        let opacity = 0.1;
+        let strokeweight = 0.5;
+        if(feature.getProperty('OBJECTID') == eventValue){
+          color = 'green';
+          opacity = 0.7;
+          strokeweight = 2;
+        }
+        return {
+          fillColor: color,
+          strokeColor: color,
+          strokeWeight: strokeweight,
+          fillOpacity:opacity
+        }
+     });
   };
 
   useEffect(() => {
     loader.load().then(async () => {
-        const { Map } = await google.maps.importLibrary("maps");
         
         map = new google.maps.Map(document.getElementById("newMap") , {
             zoom: 10,
             center: { lat: 25.276987, lng: 55.296249 },
           });
         mapRef.current = map;
-        // map.data.loadGeoJson(
-        //     "https://storage.googleapis.com/mapsdevsite/json/google.json"
-        //   );
-          // NOTE: This uses cross-domain XHR, and may not work on older browsers.
          dataHandler(
-            "https://storage.googleapis.com/mapsdevsite/json/google.json"
+            'https://prop-ai-user-data.s3.me-central-1.amazonaws.com/community.json'
           );
         setMapLoaded(true);
       });
     
-  }, []); // Empty dependency array to run only once on mount
+  }, [mapLoaded]);
 
   return (
     <div className='mapContainer'>
+          <Form.Select
+          aria-label="Default select example"
+          onChange={e =>onSelectChange(e.target.value)}>
+          { geoData && geoData.features.map((e, key) => {
+            return <option key={key} value={e.properties.OBJECTID}>{e.properties.CNAME_E}</option>;
+          })}
+          </Form.Select>
+          
           <div id='newMap' />
           <style jsx>{`
             div {
+                margin-top:20px;
                 width:95%;
                 height:95%;
                 position:fixed;
